57b601c430dc478bdcc834475b2f5685d3ea024f
ogaryou <57928285+ogaryou@users.noreply.github.com>
2020-07-19 01:36:26 +0900

    rikuest

---
+++ DAWNSNS/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('users')->insert([
+            ['username' => 'ryo','mail' => 'ogar051804@gmail.com','password' => 'Jyu2148Ryo']
+        ]);
+    }
+}
